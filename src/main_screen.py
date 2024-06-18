982b295480e67c3da896a19f20e4aca91c07b15e
unknown <joseabrahammaciastorres@gmail.com>
2024-06-18 11:04:38 -0500

    Exerciese numer 1 Acomplish | pending create logic to build bucle

--- src/main_screen.py
+++ src/main_screen.py
@@ -1,15 +1,17 @@
 import tkinter as tk
-from tkinter import messagebox
+from tkinter import Toplevel, messagebox
 from PIL import Image, ImageTk
+import io
 import requests
-import os
 from src.api import RickAndMortyAPI
+import os
 
 class MainScreen:
     def __init__(self, root):
         self.root = root
         self.root.title("Rick and Morty App - Main")
         self.create_main_screen()
+        self.image_refs = []  # Lista para mantener referencias a ImageTk.PhotoImage
 
     def create_main_screen(self):
         self.main_frame = tk.Frame(self.root)
@@ -21,9 +23,6 @@ class MainScreen:
         self.character_list = tk.Listbox(self.main_frame)
         self.character_list.pack(pady=10)
         self.character_list.bind("<<ListboxSelect>>", self.display_character)
-
-        self.character_image = tk.Label(self.main_frame)
-        self.character_image.pack(pady=10)
 
     def fetch_characters(self):
         try:
@@ -42,32 +41,48 @@ class MainScreen:
         character = self.characters[selected_index]
 
         try:
-            print(f"Fetching image from: {character['image']}")  # Línea de depuración
+            print(f"Fetching image from: {character['image']}") 
+            
             image_data = RickAndMortyAPI.fetch_character_image(character['image'])
+            
+            print(f"image_data: {image_data[:20]} ...")
 
             # Guardar la imagen localmente
             image_path = f"assets/images/{character['name'].replace(' ', '_')}.png"
             os.makedirs(os.path.dirname(image_path), exist_ok=True)
             with open(image_path, 'wb') as f:
                 f.write(image_data)
-            print(f"Image saved at: {image_path}")  # Línea de depuración
+            print(f"Image saved at: {image_path}")
 
             # Mostrar la imagen en la interfaz utilizando PIL
-            image = Image.open(image_path)
-            image = image.resize((200, 200), Image.LANCZOS)  # Redimensionar la imagen
-            image.show()
+            self.buildWindowCharacter(character, image_path)
 
-            character_info = self.format_character_display(character)
-            messagebox.showinfo("Character Info", character_info)
         except requests.RequestException as e:
-            from src.utils import log_error  # Importación diferida
+            from src.utils import log_error
             log_error(f"Image Error: {e}")
             messagebox.showerror("Image Error", f"Failed to fetch image: {e}")
         except Exception as e:
             print(f"Unexpected error: {e}")
             messagebox.showerror("Error", f"Failed to display image: {e}")
 
+    def buildWindowCharacter(self, character, image_path):
+        new_window = Toplevel(self.root)
+        new_window.title(character['name'])
 
+        info_frame = tk.Frame(new_window)
+        info_frame.pack(pady=10)
+
+        image = Image.open(image_path)
+        image = image.resize((200, 200), Image.LANCZOS)
+        photo = ImageTk.PhotoImage(image)
+
+        character_info_label = tk.Label(info_frame, text=self.format_character_display(character), justify=tk.LEFT)
+        character_info_label.grid(row=0, column=0, padx=10)
+
+        character_image_label = tk.Label(info_frame, image=photo)
+        character_image_label.grid(row=0, column=1, padx=10)
+        character_image_label.image = photo  # Mantener referencia
+        
     @staticmethod
     def transform_character_data(character):
         return {
